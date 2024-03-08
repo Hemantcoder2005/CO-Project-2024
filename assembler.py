c2880589b80f81f861ebb080ac73e805bb89b031
Hemantcoder2005 <hemantnarula2203@gmail.com>
2024-03-08 19:49:49 +0530

    R_TYPE_ERROR_FIXED

--- assembler.py
+++ assembler.py
@@ -2,17 +2,9 @@
 input_file=open("input.txt")
 instruct_input=input_file.readlines()
 
-def Error_log(error_log):
-    f=open("Error.txt",'w')
-    f.write(error_log)
-    f.close()
-def cmd_Splitter(cmd):
-    temp=cmd.split(', ')
-    a=temp[0].split()
-    a.extend(temp[1::])
-    a=[item.strip() for item in a]
-    return a
+# Instruction Format
 class R_TYPE:
+    '''Handle R_TYPE Instructions'''
     def __init__(self,instruct) -> None:
         self.format="R"
         self.opcode="0110011"
@@ -28,37 +20,49 @@ class R_TYPE:
         # Handling , in register
         if(len(self.registers)!=3):
             Error_log(f"3 register are required for {self.format} Type of instruction.")
-            exit()
+            
         for reg in self.registers:
             if(int(reg[1:])) not in range(0,31):
                 Error_log(f"{reg} not a valid register")
-                exit()
-
     def toMachineCode(self):
         '''Converting to Machine Code'''
         # f1 and f2
-        f1,f2=self.f1_f2[self.instruct[0]]
+        f1,f2=self.f1_f2[self.instruct[0].lower()]
 
         # register opcode
         for i in range(len(self.registers)):
-            self.registers[i]=register_opcode_finder(int(self.registers[i][1:]),5)
+            self.registers[i]=opcode_finder(int(self.registers[i][1:]),5)
         return self.opcode+" "+ self.registers[0]+" "+f1+self.registers[1]+" "+self.registers[2]+" "+f2
 
-def register_opcode_finder(reg,no_of_bits):
-    '''We have int value to convert into binary with no. of bits'''    
-    binary_opcode = format(reg, f'0{no_of_bits}b')
-    return binary_opcode
 
 # Important Functions
+def Error_log(error_log):
+    '''This will create Error.txt to display error'''
+    f=open("Error.txt",'w')
+    f.write(error_log)
+    f.close()
+    exit()
+def cmd_Splitter(cmd):
+    '''This will convert command in a list'''
+    temp=cmd.split(', ')
+    a=temp[0].split()
+    a.extend(temp[1::])
+    a=[item.strip() for item in a]
+    return a
 def typeChecker(cmd):
     '''Check the type of Instruction'''
     a=cmd.split()
     instruct=a[0].lower()
     if instruct in ['add','sub','slt','sltu','xor','sll','srl','or','and']:
         return R_TYPE(cmd)
+    # elif instruct =="jal"
     else:
         Error_log(f"{instruct} not a valid instruction")
-        exit()
+def opcode_finder(reg,no_of_bits):
+    '''We have int value to convert into binary with no. of bits'''    
+    binary_opcode = format(reg, f'0{no_of_bits}b')
+    return binary_opcode
+
 
 
 instruction=[]
